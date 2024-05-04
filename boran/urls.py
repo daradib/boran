c613b5f6387068e03f8bf4f3b448a8eb3111aee2
Dara Adib <daradib@ocf.berkeley.edu>
2024-05-03 22:38:03 -0400

    Remove Sentry user feedback
     - sentry-python no longer supports API function last_event_id()
       https://docs.sentry.io/platforms/python/migration/1.x-to-2.x
     - EasyPrivacy content filtering blocks ingest.sentry.io

--- boran/urls.py
+++ boran/urls.py
@@ -1,8 +1,5 @@
-from django.conf import settings
 from django.contrib import admin
-from django.shortcuts import render
 from django.urls import include, path
-from sentry_sdk import last_event_id
 
 
 urlpatterns = [
@@ -10,14 +7,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('phonebank/', include('phonebank.urls'), name='phonebank'),
 ]
-
-
-def handler500(request, *args, **argv):
-    """Handle errors with Sentry user feedback.
-
-    https://docs.sentry.io/enriching-error-data/user-feedback/?platform=django
-    """
-    return render(request, '500.html', {
-        'sentry_dsn': settings.SECRETS['SENTRY_DSN'],
-        'sentry_event_id': last_event_id(),
-    }, status=500)
